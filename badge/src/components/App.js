c7022b66b11acc769424ed4fc541de9675ec5852
Rubén <rubenzc@me.com>
2018-12-06 20:30:18 +0100

    Added header component

--- badge/src/components/App.js
+++ badge/src/components/App.js
@@ -1,22 +1,14 @@
 import React, { Component } from 'react';
+import Header from './Header';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <p>
-            TEST
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="contenedor">
+        <Header
+          titulo = 'Weekly badge'
+        />
       </div>
     );
   }
