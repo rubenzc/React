b7fbe7e3546b3957aad374e121909fcf43676d98
Rubén <rubenzc@me.com>
2018-12-11 17:38:21 +0100

    Added header

--- dates/src/App.js
+++ dates/src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './index.css';
+import Header from './components/Header';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-
+      <div className="container">
+        <Header
+          title = {'Veterinary patient administrator'}
+        />
       </div>
     );
   }
