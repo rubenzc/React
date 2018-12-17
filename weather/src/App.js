43723a985dd6a8ce676b7e44e3c5e9199cb7229e
Rubén <rubenzc@me.com>
2018-12-17 19:25:34 +0100

    Added error component

--- weather/src/App.js
+++ weather/src/App.js
@@ -1,16 +1,48 @@
 import React, { Component } from 'react';
 import Header from './components/Header';
 import Form from './components/Form';
+import Error from './components/Error';
 
 class App extends Component {
+
+  state = {
+    error: ''
+  }
+
+  componentDidMount(){
+    this.setState({
+      error: false
+    })
+  }
+
+  dataSearch = answer =>{
+    
+    if(answer.city === '' || answer.country === ''){
+      this.setState({
+        error: true
+      })
+    }
+  }
+
   render() {
+
+    const error = this.state.error;
+
+    let result;
+
+    if(error){
+      result = <Error message="The fields can't be empty"/>
+    }
+
     return (
       <div className="app">
         <Header
           title = 'React weather'
         />
         <Form
+          dataSearch = {this.dataSearch}
         />
+        {result}
       </div>
     );
   }
--- weather/src/components/Form.js
+++ weather/src/components/Form.js
@@ -2,18 +2,37 @@ import React, { Component } from 'react';
 
 class Form extends Component {
     
+    //Create refs
+    cityRef = React.createRef();
+    countryRef = React.createRef();
+
+    answerSearch = (e) => {
+        e.preventDefault();
+
+        //Read refs and create the object
+        const answer = {
+            city: this.cityRef.current.value,
+            country: this.countryRef.current.value
+        }
+    //Send the props
+        this.props.dataSearch(answer);
+
+    //Reset
+    e.currentTarget.reset();
+    }
+
     render() { 
         return (
             <div className="contenedor-form">
                 <div className="container">
                     <div className="row">
-                        <form>
+                        <form onSubmit={this.answerSearch}>
                             <div className="input-field col s12 m8 l4 offset-m2">
-                                <input id="city" type="text" />
+                                <input ref={this.cityRef} id="city" type="text" />
                                 <label htmlFor="city">City:</label>
                             </div>
                             <div className="input-field col s12 m8 l4 offset-m2">
-                                <select>
+                                <select ref={this.countryRef}>
                                     <option value="" defaultValue>Choose a country:</option>
                                     <option value="AR">Argentina</option>
                                     <option value="CO">Colombia</option>
