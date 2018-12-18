dce4e7a724de1b4f5d420eac13daf3db7d67a0c8
Rubén <rubenzc@me.com>
2018-12-18 16:59:40 +0100

    Showing final results

--- weather/src/components/Weather.js
+++ weather/src/components/Weather.js
@@ -9,9 +9,25 @@ class Weather extends Component {
 
         if(!name || !weather || !main) return null;
 
+        const kelvin = 273.15;
+        const urlIcon = `http://openweathermap.org/img/w/${weather[0].icon}.png`;
+        const alt = `${name} Weather`;
         return(
             <div className="row">
-                {name}
+                <div className="resultado col s12 m8 l6 offset-m2 offset-l3">
+                    <div className="card-panel light-blue align-center">
+                        <span className="white-text">
+                            <h2> Weather in {name}</h2>
+                            <p className="temperatura">
+                                Now: { (main.temp - kelvin).toFixed(1) }&deg;C
+                                <img src={urlIcon} alt={alt} />
+                            </p>
+                            <p>Max. { (main.temp_max - kelvin).toFixed(1) }&deg;C</p>
+                            <p>Min. { (main.temp_min - kelvin).toFixed(1) }&deg;C</p>
+                        </span>
+
+                    </div>
+                </div>
             </div>
         )
 
